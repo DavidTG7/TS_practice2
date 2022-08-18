24caa3b4a1592266ce072a15cdcd05daa7bf07ab
DavidTG7 <davidthekoa@gmail.com>
2022-08-18 18:28:20 -0500

    Practice with number type

---
+++ src/04-numbers.ts
@@ -0,0 +1,9 @@
+(()=>{
+  let productPrice = 100;
+  const twentyDisc = '20';
+  let discount = parseInt(twentyDisc);
+  let totalPrice = productPrice - productPrice * discount / 100;
+
+  console.log('Total price: ', totalPrice, typeof totalPrice);
+
+})();
