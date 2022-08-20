ec4a19d9252e30ad24c616cde18cbec42d06429e
DavidTG7 <davidthekoa@gmail.com>
2022-08-20 18:12:15 -0500

    Strings on TS

---
+++ src/06-strings.ts
@@ -0,0 +1,20 @@
+(() => {
+  let productName = 'My product';
+  console.log(productName);
+
+  let myName: string;
+
+  myName = "David";
+
+  console.log(myName);
+
+  let summary = `
+    Hi,
+    I'm David!
+    A software Engineer!
+    ${productName}
+  `
+  console.log(summary);
+
+
+})();
